4daa090eac9bf0af43eb84cb435741e18d95e192
Lorenz <lorenz.neidhart@aon.at>
2019-01-21 12:56:11 +0100

    non-runnable with yahoo-data

--- app.R
+++ app.R
@@ -23,28 +23,28 @@ ui <- fluidPage(
 			column(6,
 				textInput("stock2", "Stock 2", "")),
 			column(5,
-				numericInput("w1", "Portf. %", 20, 
+				numericInput("w2", "Portf. %", 20, 
 				   min = 1, max = 100))
 		),
 		fluidRow(
 			column(6,
 				textInput("stock3", "Stock 3", "")),
 			column(5,
-				numericInput("w1", "Portf. %", 20,
+				numericInput("w3", "Portf. %", 20,
 				   min = 1, max = 100))
 		),
 		fluidRow(
 			column(6,
 				textInput("stock4", "Stock 4", "")),
 			column(5,
-				numericInput("w1", "Portf. %", 20,
+				numericInput("w4", "Portf. %", 20,
 				   min = 1, max = 100))
 		),
 		fluidRow(
 			column(6,
 				textInput("stock5", "Stock 5", "")),
 			column(5,
-				numericInput("w1", "Portf. %", 20, 
+				numericInput("w5", "Portf. %", 20, 
 				   min = 1, max = 100))
 		),
 		fluidRow(
@@ -87,21 +87,41 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
   
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  
   observeEvent(input$sumbit, {
+    getStockPrices();
     session$sendCustomMessage(type = 'testmessage',
                               message = 'Thank you for clicking')
   })
+  plot(stockPrices())
+  
   
 }
 
+
+
+
 # Create Shiny app ----
 shinyApp(ui = ui, server = server)
+
+
+
+# Load Historic Data from Yahoo
+# Args Stockname, fromDate, toDate
+getStockPrices <- function(){
+  
+  environment<-new.env()
+  stock<-"SPY"
+  
+  options("getSymbols.yahoo.warning"=FALSE)
+  getSymbols(stock,
+             env = environment,
+             src = "yahoo",
+             from = as.Date("2009-01-01"),
+             to = as.Date("2019-01-01"))
+  
+  output<-get(stock, envir = environment)
+  
+  closingPrices<-Cl(output)
+  
+  return (closingPrices) 
+}
